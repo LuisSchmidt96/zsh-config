cf6daf9405200d3cd4d393ed405152b047dc07aa
Luis Schmidt <schmidt@amiconsult.de>
2024-09-18 18:09:59 +0200

    Update to setup.sh and update.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-cp ./.zshrc ~
-cp ./.oh-my-zsh ~
-source ~/.zshrc
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-echo "Zsh config setup complete!"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "Setting up Zsh config"
+
+if [ -f "$HOME/.zshrc" ]; then
+    echo "Backing up existing .zshrc to .zshrc.bak"
+    mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+fi
+
+ln -s "$SCRIPT_DIR"/.zshrc "$HOME"/.zshrc
+echo "Symlinked .zshrc from $SCRIPT_DIR to $HOME/.zshrc"
+
+if [ -d "$HOME/.oh-my-zsh/custom" ]; then
+  echo "Setting up custom plugins and themes..."
+
+  # Copy custom plugins and themes from the repo
+  rsync -a --exclude='.git' "$SCRIPT_DIR/custom/" "$HOME/.oh-my-zsh/custom/"
+  
+  echo "Custom plugins and themes have been set up."
+else
+  echo "Error: ~/.oh-my-zsh/custom directory does not exist!"
+fi
+
+# Source the new .zshrc to apply changes
+source "$HOME/.zshrc"
+
+echo "Zsh setup complete!"
--- update.sh
+++ update.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-cp ~/.zshrc .
-cp ~/.oh-my-zsh .
+# Get the directory where the script is located
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Echo the script directory
+echo "Script directory: $SCRIPT_DIR"
+
+# Check if .zshrc exists and then copy it
+if [ -f "$HOME/.zshrc" ]; then
+  cp "$HOME/.zshrc" "$SCRIPT_DIR"
+  echo ".zshrc copied to $SCRIPT_DIR"
+else
+  echo ".zshrc not found in $HOME"
+fi
+
+# Copy the custom oh-my-zsh directory
+if [ -d "$HOME/.oh-my-zsh/custom" ]; then
+  rsync -a --exclude ".git" "$HOME/.oh-my-zsh/custom" "$SCRIPT_DIR"
+  echo "Custom Oh-My-Zsh directory copied to $SCRIPT_DIR"
+else
+  echo "Custom Oh-My-Zsh directory not found in $HOME"
+fi
 
 echo "Zsh config update complete!"
